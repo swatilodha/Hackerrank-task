0c69c1b4319d73f742c52164d542c24398dd6c60
SWATI LODHA <swatilodha27@gmail.com>
2017-11-14 03:22:00 +0530

    Update loop.h

--- loop.h
+++ loop.h
@@ -1,16 +1,14 @@
-#include<stdio.h>
-#include<time.h>
+#include<iostream>
+using namespace std;
 
-double loop(clock_t t){
-    printf("Entering while loop..\n");
-    double time_taken = 0.00;
+void loop(){
+    cout << "Entering while loop..\n";
+    //double time_taken = 0.00;
     
     while(1){
-        t = clock() - t;
-        time_taken = ((double)t)/CLOCKS_PER_SEC;
-        if(time_taken > 3.00)
-            return 4.00;
+        
     }
     
-    return time_taken;
+    //return time_taken;
+    return;
 }
